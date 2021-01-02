a73b9c4065c06ff489a54c82d81224be0ee56de9
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2021-01-02 18:45:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+    return(
+        <div>
+            <h1 className="title">TOWEL DISPENSER</h1>
+        </div>
+    )
+}
+
+export default App;

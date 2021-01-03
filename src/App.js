43f5890407211fc9769bba67aca37887b0d542da
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2021-01-02 19:20:33 -0500

    fixing the stripe stuff@

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   } from '@stripe/react-stripe-js';
 
   
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
 exports.handler = async (event) => {
   try {
